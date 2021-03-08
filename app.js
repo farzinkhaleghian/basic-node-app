f28c6d85667edddc9545827b97bda5f797fe554a
farzinkhaleghian <fkhlaleghian@gmail.com>
2021-03-08 17:48:46 -0600

    express added

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const config = require('config');
 const logger = require('./startup/logging');
+const express = require('express');
 
 logger.info(process.env.NODE_ENV);
-const p = Promise.reject(new Error('THISIS Faild'));
+
+const app = express();
+
+const port = process.env.PORT || config.get('port');
+app.listen(port, () => {
+    logger.info(`Running on port ${port}`);
+})
