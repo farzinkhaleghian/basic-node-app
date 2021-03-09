027a3974841a2400bf858a2ad430e5679fddb593
farzinkhaleghian <fkhlaleghian@gmail.com>
2021-03-08 23:18:51 -0600

    json parser added

--- startup/routing.js
+++ startup/routing.js
@@ -3,6 +3,7 @@ const routes = require('../routes/router');
 const error = require('../middleware/error');
 
 module.exports = (app) => {
+    app.use(express.json());
     app.use('/api', routes);
     app.use(error);
 };
