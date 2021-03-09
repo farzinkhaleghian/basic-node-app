39eb2edda2ecfb4040bc4f67db355585a6876bad
farzinkhaleghian <fkhlaleghian@gmail.com>
2021-03-08 23:22:11 -0600

    cors added

--- startup/routing.js
+++ startup/routing.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('../routes/router');
 const error = require('../middleware/error');
+const cors = require('cors');
 
 module.exports = (app) => {
     app.use(express.json());
+    app.use(cors());
     app.use('/api', routes);
     app.use(error);
 };
